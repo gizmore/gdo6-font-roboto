b02b785a75bb3735d16b22de1a6febccfd6f4f6f
gizmore <gizmore@wechall.net>
2020-10-28 15:34:29 +0100

    6.10-r9326

--- Module_FontRoboto.php
+++ Module_FontRoboto.php
@@ -31,22 +31,36 @@ final class Module_FontRoboto extends GDO_Module
     {
         return array(
             GDT_FontWeight::make('font_weight')->enumValues('100', '200', '300', '400', '500', '700', '900')->initial('400'),
+            GDT_FontWeight::make('font_weight_mono')->enumValues('100', '200', '300', '400', '500', '600', '700')->initial('400'),
             GDT_Checkbox::make('font_include')->initial('1'),
+            GDT_Checkbox::make('font_include_mono')->initial('1'),
         );
     }
     public function cfgFontWeight() { return $this->getConfigVar('font_weight'); }
+    public function cfgFontWeightMono() { return $this->getConfigVar('font_weight_mono'); }
     public function cfgIncludeFont() { return $this->getConfigVar('font_include'); }
+    public function cfgIncludeFontMono() { return $this->getConfigVar('font_include_mono'); }
     
     ###########
     ### CSS ###
     ###########
     public function onIncludeScripts()
     {
+        # Always load normal font
         $weight = $this->cfgFontWeight();
         $this->addBowerCSS("fontsource-roboto/{$weight}.css"); # Font face
+
+        # Expose as default?
         if ($this->cfgIncludeFont())
         {
             $this->addCSS('css/gdo6-roboto.css'); # Body style
+        }
+    
+        # Optionally load mono too
+        if ($this->cfgIncludeFontMono())
+        {
+            $weight = $this->cfgFontWeightMono();
+            $this->addBowerCSS("fontsource-roboto-mono/{$weight}.css"); # Font face
         }
     }
     
